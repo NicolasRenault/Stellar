32d147aa43ae6a26779fdf209acaa6f7606d83b8
Nicolas Renault <contact.nicolasrenault@gmail.com>
2022-04-03 17:58:09 +0200

    Setup Firestore

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import './style.css'
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut} from "firebase/auth";
+import { getFirestore, collection, addDoc, setDoc, doc } from "firebase/firestore";
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyDN3EXcPw6y1FAlVJX0t2HYYrekKAaMicE",
@@ -13,23 +15,44 @@ const firebaseConfig = {
 
 const firebase = initializeApp(firebaseConfig);
 const auth = getAuth(firebase);
+const db = getFirestore(firebase);
 const provider = new GoogleAuthProvider();
 
 const signInBtn = document.getElementById("signInBtn");
 const signOutBtn = document.getElementById("signOutBtn");
 const whenSignedIn = document.getElementById("whenSignedIn");
 const whenSignedOut = document.getElementById("whenSignedOut");
+const userDetails = document.getElementById('userDetails');
+const editUserSettings = document.getElementById("editUserSettings");
 
 signInBtn.onclick = () => signInWithPopup(auth, provider).catch((error) => console.log('error'));
 signOutBtn.onclick = () => signOut(auth);
 
 auth.onAuthStateChanged(user => {
-    console.log(user)
     if (user) {
         whenSignedIn.hidden = false;
         whenSignedOut.hidden = true;
+        userDetails.innerHTML = `<h3>Hello ${user.displayName}!</h3> <p>User ID: ${user.uid}</p>`;
+        
     } else {
         whenSignedIn.hidden = true;
         whenSignedOut.hidden = false;
+        userDetails.innerHTML = '';
     }
 });
+
+
+// editUserSettings.onclick = () => {
+//     try {
+//         const docRef = setDoc(doc(db, "user-settings", user.uid), {
+//             uid: user.uid,
+//             week_sleep_time : "1000",
+//             week_wake_time : "0730",
+//             weekend_sleep_time : "0130",
+//             weekend_wake_time : "0945"
+//         });
+//         console.log("Document written with ID: ", docRef.id);
+//     } catch (e) {
+//         console.error("Error adding document: ", e);
+//     }
+// }
