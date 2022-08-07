aacc6df1e9962f7a4971283bdd915abb87f55427
Nicolas Renault <contact.nicolasrenault@gmail.com>
2022-08-07 14:02:05 +0200

    Adding starry night, Changing day gradient

--- sky.js
+++ sky.js
@@ -17,9 +17,12 @@ switchBtn.onclick = () => {
     }
 }
 
+initStars();
+
 function setDarkMode() {
     document.body.classList.remove("light");
     document.body.classList.add("dark");
+    initStars();
 
     switchSvg.src = darkSVG;
 }
@@ -27,6 +30,36 @@ function setDarkMode() {
 function setLightMode() {
     document.body.classList.remove("dark");
     document.body.classList.add("light");
+    clearStars();
     
     switchSvg.src = lightSVG;
+}
+
+/**
+ * Create {window.innerWidth / 10} starts randomly placed 
+ */
+function initStars() {
+    let count = window.innerWidth / 10;
+
+    for (let i = 0; i < count; i++) {
+        let star = document.createElement("i");
+        let x = Math.floor(Math.random() * 100);
+        let y = Math.floor(Math.random() * 100);
+        let size = Math.random() * 2;
+
+        star.style.left = x + "%";
+        star.style.top = y + "%";
+        star.style.width = 1 + size + "px";
+        star.style.height = 1 + size + "px";
+        star.classList.add("stars");
+
+        skyContainer.appendChild(star);
+    }
+}
+
+/**
+ * Remove all the stars
+ */
+function clearStars() {
+    skyContainer.innerHTML = '';
 }
